81ae3e65c104a0e952de88a6ff5a2636d14d9894
Pinki Jha <pinkijha1310@gmail.com>
2024-12-20 03:21:59 +0530

    add component structure on all and pending

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const All = () => {
   return (
-    <div>
-      <h1>All</h1>
+    <div
+      className="flex flex-col px-4  overflow-y-scroll md:my-4 p-4 space-y-2 md:w-[700px] w-[350px] h-[440px] md:h-[400px]
+                 md:bg-purple-100 md:shadow-sm scrollbar-hide md:shadow-green-200 md:rounded-xl"
+    >
+      <h2 className="text-lg md:text-black text-purple-200 font-bold mb-4">All Tasks</h2>
+     
     </div>
   )
 }
--- src/components/Pending.jsx
+++ src/components/Pending.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Pending = () => {
   return (
-    <div>
-      
+    <div
+      className="flex flex-col px-4  overflow-y-scroll md:my-4 p-4 space-y-2 md:w-[700px] w-[350px] h-[440px] md:h-[400px]
+                 md:bg-gray-100 md:shadow-sm scrollbar-hide md:shadow-green-200 md:rounded-xl"
+    >
+      <h2 className="text-lg md:text-black text-gray-200 font-bold mb-4">Pending Tasks</h2>
+     
     </div>
   )
 }
